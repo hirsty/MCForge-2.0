f38fd57e53792a0a70a1d11478afdf22c2cbad39
cazzar <cayde.dixon@gmail.com>
2012-02-26 11:09:35 +1100

    started database and layout is there

---
+++ MCForge2_/Database/MySQLHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+
+namespace MCForge2_.Database
+{
+    class MySQLHelper
+    {
+        private MySqlConnection connection = null;
+        private MySqlTransaction transaction = null;
+        private string MySQLconnString = null;
+        /// <summary>
+        /// Executes the query in MySQL.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <returns>true if executed without errors, false otherwise</returns>
+        public static bool Execute(string query)
+        {
+            try
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                }
+            }
+            return true;
+        }
+    }
+}
